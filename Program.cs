661e1ba33ea214d5a4b8aeeb9dce5753f20ac76d
Hamza Qureshi <m.hamzaqureshi15@gmail.com>
2022-12-12 15:35:18 +0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            
+            for(int i=0; i< 10; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Print();
+        }
+    }
+}
